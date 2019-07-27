9c61674a0c2f6f4c9188ad981f44d3992f150d63
Lloyduct <52854585+Lloyduct@users.noreply.github.com>
2019-07-27 13:58:56 +0300

    Delete create_image.sh

--- create_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-declare -r IMAGE_NAME="miguno/golang-docker-build-tutorial"
-declare -r IMAGE_TAG="latest"
-
-echo "Building image '$IMAGE_NAME:$IMAGE_TAG'..."
-docker build -t $IMAGE_NAME:$IMAGE_TAG .

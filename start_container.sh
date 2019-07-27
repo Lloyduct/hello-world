d52ef1e6eca6fc1a21c02bbabe45dfdacdbd506f
Lloyduct <52854585+Lloyduct@users.noreply.github.com>
2019-07-27 13:58:58 +0300

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-declare -r IMAGE_NAME="miguno/golang-docker-build-tutorial"
-declare -r IMAGE_TAG="latest"
-declare -r APP_PORT="8123"
-
-echo "Starting container for image '$IMAGE_NAME:$IMAGE_TAG', exposing port $APP_PORT/tcp"
-docker run -p $APP_PORT:$APP_PORT $IMAGE_NAME:$IMAGE_TAG
